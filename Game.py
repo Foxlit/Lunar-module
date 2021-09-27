b5b22b07fe62ed891b76d7ee760a2671f4516b6e
Foxlit <v.gromov2006@gmail.com>
2021-09-27 21:07:18 +0300

    Update Game.py

--- Game.py
+++ Game.py
@@ -1,12 +1,18 @@
 def check_input(input_fuel):
     try:
+        minus_fuel = fuel - input_fuel
         int_input = abs(float(input_fuel))
         valid_result = True
     except ValueError:
         print("Некорректный ввод! Введите число")
         int_input = 0
         valid_result = False
-    return int_input, valid_result
+    return int_input, valid_result, minus_fuel
+
+
+def control_remeing_fuel(fuel):
+    if fuel < 0:
+        return False
 
 
 fuel = 1000
@@ -17,9 +23,14 @@ mass = 15000
 burnt_fuel = 0
 k = 15
 validation = True
+print(f' Добро Пожаловать в Lunar Module!\n Здесь тебе предстоит посадить космический модуль на луну!\n '
+      f'Вот начальные данные, чтобы сориентироваться:\n Топливо:  {fuel}\n Твоя начальная скорость:  {speed}\n'
+      f' Твоя текущая высота: {height}\n ну что, ПОЕХАЛИ!')
+
 while height > 0:
-    input_fuel = input("Сколько топлива сжечь? ")
-    burnt_fuel, validation = check_input(input_fuel)
+    input_fuel = input(" Сколько топлива сжечь? ")
+    burnt_fuel, validation = check_input(int(input_fuel))
+
     if validation:
         fuel -= burnt_fuel
         speed += (2 * g * height) ** 0.5 - burnt_fuel * k
@@ -27,6 +38,7 @@ while height > 0:
 
 # TODO: сделать расчет остатка топлива. Не должно быть отрицательного
 
+        print("У вас нет столько топлива!")
         print("Топлива сожжено: ", burnt_fuel, "кг")
         print("Топлива осталось: ", fuel, "кг")
         if speed < 0:
