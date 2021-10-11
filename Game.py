62b15c5cd2b3c2294a5d850f810bcf087ca7af54
Foxlit <v.gromov2006@gmail.com>
2021-10-11 21:14:07 +0300

    #7 resolved 1. Добавлен счетчик ходов 2. Изменен алгоритм расчета скорости

--- Game.py
+++ Game.py
@@ -1,6 +1,11 @@
 import values
 
 
+burnt_fuel = 0
+validation = True
+end_game = False
+
+
 def check_input(curr_fuel_portion):
     float_input_fuel = 0
     try:
@@ -22,9 +27,9 @@ def control_remaining_fuel(curr_fuel):
 
 
 def check_flight_status(curr_height, curr_speed):
-    if 0 > curr_height > (-30) and curr_speed < 5000:
+    if 0 > curr_height > (-30) and curr_speed < 50:
         return 1  # successful landing
-    elif curr_height < (-3):
+    elif curr_height < (-10):
         return -1  # crashed
     else:
         return 0  # still flying
@@ -32,8 +37,8 @@ def check_flight_status(curr_height, curr_speed):
 
 def make_calculations(curr_height, curr_speed, curr_fuel, curr_fuel_portion):
     curr_fuel -= curr_fuel_portion
-    curr_speed += (2 * g * curr_height) ** 0.5 - curr_fuel_portion * k
-    curr_height -= curr_speed * 60
+    curr_speed += g - curr_fuel_portion * k
+    curr_height -= curr_speed
     return curr_height, curr_speed, curr_fuel
 
 
@@ -50,28 +55,24 @@ def check_level_input(chosen_level):
             correct_level = input('Некорректный выбор. Выберите от 1 до 3: ')
 
 
-burnt_fuel = 0
-validation = True
-end_game = False
 
-print(f' Добро Пожаловать в Lunar Module!\n Здесь тебе предстоит посадить космический модуль на луну!\n ')
+print(f' Добро Пожаловать в Lunar Module!\n Здесь тебе предстоит посадить космический модуль на Луну!\n ')
 level = check_level_input(input('Введите уровень от 1 до 3: '))
 
 fuel, speed, height, g, mass, k = values.get_values_by_level(level)
 
 
-print(f'Вот начальные данные, чтобы сориентироваться:\n Топливо:  {fuel} кг\n Твоя начальная скорость:  {speed}\n'
-      f' Твоя текущая высота: {height}\n ну что, ПОЕХАЛИ!')
+print(f'Вот начальные данные, чтобы сориентироваться:\nТопливо:  {fuel} кг\nТвоя начальная скорость:  {speed}\n'
+      f'Твоя текущая высота: {height}\nну что, ПОЕХАЛИ!\n===============================')
 
-
+motion = 1
 while not end_game:
-    input_fuel = input(" Сколько топлива сжечь? ")
+    input_fuel = input("Сколько топлива сжечь? ")
     burnt_fuel, validation = check_input(input_fuel)
 
     if validation:
 
         height, speed, fuel = make_calculations(height, speed, fuel, burnt_fuel)
-
 
         print("Топлива сожжено: ", burnt_fuel, "кг")
         print("Топлива осталось: ", fuel, "кг")
@@ -84,10 +85,14 @@ while not end_game:
             print("Зависание")
 
         print("Текущая высота: ", round(height, 3), "м")
+        print(f'Сейчас {motion} ход')
         print("===============================")
+
+        motion += 1
 
         if check_flight_status(height, speed) == 1:
             print("Вы успешно посадили корабль!")
+            print(f'Вы справились с задчей за {motion} шагов')
             end_game = True
         elif check_flight_status(height, speed) == -1:
             print("Космический модуль разбился!")
--- values.py
+++ values.py
@@ -6,29 +6,25 @@ def get_values_by_level(chosen_level):
         speed = 0
         height = 1000
         g = 1.62
-        mass = 15000
-        burnt_fuel = 0
+        mass = 8000
         k = 0.1
 
     # Level 2
-    if chosen_level == 2:
+    elif chosen_level == 2:
+        fuel = 1500
+        speed = 0
+        height = 1000
+        g = 1.62
+        mass = 15000
+        k = 0.3
+
+    # Level 3
+    else:
         fuel = 1000
         speed = 0
         height = 1000
         g = 1.62
-        mass = 15000
-        burnt_fuel = 0
-        k = 0.1
-
-    # Level 3
-    if chosen_level == 3:
-        fuel = 500
-        speed = 0
-        height = 1000
-        g = 1.62
-        mass = 15000
-        burnt_fuel = 0
-        k = 0.1
+        mass = 20000
+        k = 0.5
 
     return fuel, speed, height, g, mass, k
-
